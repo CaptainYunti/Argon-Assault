43643b7defcfe64c36c980eb0c5fd890b779e0e6
Yunti <n.pawcio@gmail.com>
2022-09-26 02:34:13 +0200

    Added ship explosion

---
+++ Argon Assault/Assets/Scripts/Crash.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Crash : MonoBehaviour
+{
+
+    [SerializeField] float secondsToWaitAfterCrash = 1f;
+    [SerializeField] ParticleSystem explosionParticle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.GetComponent<PlayerControls>().enabled = false;
+        explosionParticle.Play();
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer mesh in meshRenderers)
+        {
+            mesh.enabled = false;
+        }
+        GetComponent<BoxCollider>().enabled = false;
+        Invoke("ReloadScene", secondsToWaitAfterCrash);
+    }
+
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
